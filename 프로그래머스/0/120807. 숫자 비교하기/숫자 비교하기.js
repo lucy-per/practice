38f51ec2eea9f18c0c1b19708ebfa62c9fce2bc0
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-13 10:31:00 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,9 @@
+const solution = (num1, num2) => {
+    if (num1 === num2) {
+        return 1
+    } else return -1
+}
+
+// const solution = (num1, num2) => {
+// answer = num == num2 ? 1 ? -1
+// return answer; }
