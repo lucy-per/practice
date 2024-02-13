29b517d8950245e35e44dcaed011bd836568ee87
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-13 10:18:49 +0900

    Update 몫 구하기.js

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,3 +1,8 @@
 const solution = (num1, num2) => {
     return Math.floor(num1 / num2)
 }
+
+//ParseInt()를 사용할 수도 있지만 
+//bigInt에서는 String으로 출력되는 경우도 있어 
+// => Math.floor()도 안되지 않나.. 그럼.. 
+//Math.floor()를 사용
