e62a9c98160d0a14971fddcb1779de3eba95b86e
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-13 13:03:29 +0900

    Update 배열 두배 만들기.js

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -3,3 +3,10 @@ const solution = (numbers) => {
     return multipleNumbers
     // return을 하지 않으면 undefied가 되니 유의하자
 }
+
+// const를 쓰지 않고 바로 return multipleNumbers = numbers.map((value)=> value * 2 ) 도 가능
+// map이 아닌 reduce를 사용해서 나타낼 수도 있음
+// function solution(numbers) {
+//     return numbers.reduce((a, b) => [...a, b * 2], []);
+// }
+// array.reduce(콜백함수, 초기값)
