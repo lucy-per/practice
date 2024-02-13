d0dcac82e2870ef10a933ee6dac8d49996fc9e7a
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-13 10:24:19 +0900

    Update 두 수의 나눗셈.js

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -2,3 +2,9 @@ const solution = (num1, num2) => {
     const value = (num1/num2) * 1000 ;
     return Math.floor(value);
 }
+
+// 작성가능한 방법
+//  1. return Math.trunc(num1 / num2 * 1000) 
+// trunc는 소수점 이하 다 버림 vs floor는 소수점 내림
+//  2. return ~~ (num1 / num2 * 1000)
+// ~~ 은 Math.floor와 같은 기능
