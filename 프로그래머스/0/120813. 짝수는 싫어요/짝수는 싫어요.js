63b69a785dcd351ac0093361e22433c2170019a3
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-14 16:27:12 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,14 @@
+const solution = (n) => {
+    let arr = [];
+    for (let i=1 ;  i <= n ; i++) {
+        if (i % 2 === 0 ) {
+            // % 나머지 구하는 연산자
+            // / 는 단순 나눗셈
+            // i % 2 = 0으로하면 오류가 남 
+            // 비교는 반드시 === 으로 
+            continue;
+        } 
+        arr.push(i);   
+    }
+    return arr.sort((a,b)=> a - b);
+}
