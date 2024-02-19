dc88e63c5b68648a417b64e1dcb0797f0f87acba
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-19 16:14:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,9 @@
+const solution = (price) => {
+const total = 
+price >= 500000 ? price*0.8 : 
+price >= 300000 ? price*0.9 : 
+price >= 100000 ? price*0.95 : 
+price
+
+return answer = Math.floor(total);
+}
