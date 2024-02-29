c49d92376f604715c39e3b54b032742b493b6372
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-02-29 15:23:01 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -0,0 +1,5 @@
+const solution = (my_string) => {
+    return answer = [...my_string.toLowerCase()].sort().join('')
+                    //[...a]스프레드 연산자 =>split과 동일한 역할
+                    // sort() 만 쓰면 알파벳 정렬 가능
+}
