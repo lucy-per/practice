b4813b65eb2d3d20d57a21fb76548640a8137b15
lucy-per <156499446+lucy-per@users.noreply.github.com>
2024-03-05 14:00:09 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 13:58:56
+2024년 03월 05일 14:00:07
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,15 +1,9 @@
 const solution = (array, n) => {
     // 차의 절댓값이 가장 작은 값의 인덱스를 반환하기
-//     const arr = array.map((a)=> Math.abs(a-n))
-//     const minIndex = arr.indexOf(Math.min(...arr))
-//     return array[minIndex]
-// }
-    
     // 가까운 수가 여러 개인 경우 더 작은 수를 반환하기
     // => 무조건 내림차순으로 정렬한 뒤에 값 반환하기
     const newArr = array.sort((a,b)=> a-b)
     const absArr = newArr.map((a)=> Math.abs(a-n))
-    const minNum = Math.min(...absArr)
-    const minIndex = absArr.indexOf(minNum)
+    const minIndex = absArr.indexOf(Math.min(...absArr))
     return newArr[minIndex] 
 }
